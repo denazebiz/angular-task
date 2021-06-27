b2eb51df4549d36cb74d2d2c8e03af0439923b1c
den <menacedev@gmail.com>
2021-06-27 17:57:26 +0100

    feat: app module changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        loadChildren: () => import('./pages/jobs/jobs.module').then(m => m.JobsModule)
+      }
+    ]),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
